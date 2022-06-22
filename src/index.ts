81cfb3a0fe6178ea8b901bc9e40908de2caa4c7a
disguised-uchiha <harsh.boricha2015@gmail.com>
2022-06-22 11:30:00 +0530

    fix: rollup fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
-export * from "./BiTag/BiTag";
-export * from "./BiTooltip/BiTooltip";
-export * from "./BiDropDown/BiDropDown";
-export * from "./BiRadio/BiRadio";
-export * from "./BiAlertBox/BiAlertBox";
 export * from "./TestComp/TestComp";
